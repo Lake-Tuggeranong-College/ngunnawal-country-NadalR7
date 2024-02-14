a72b895ac1ab05fda4eaf7d096d7fef3c3c2801a
NadalR7 <159747302+NadalR7@users.noreply.github.com>
2024-02-14 09:55:53 +0000

    new routes

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'nadal is the best'
+    return render_template("index.html", title="Homepage")
 
+@app.route('/history')
+def history():  # put application's code here
+    return render_template("history.html", title="History of Ngunnawal")
+
+@app.route('/imagegallery')
+def imagegallery():  # put application's code here
+    return render_template("imagegallery.html", title="Image Gallery")
+
+@app.route('/contactus')
+def contactus():  # put application's code here
+    return render_template("contactus.html", title="Contact Us")
 
 if __name__ == '__main__':
     app.run()
