530972360f302a12b1fbfa8b2558634ac0cc1833
NadalR7 <159747302+NadalR7@users.noreply.github.com>
2024-05-02 07:44:28 +0000

    account changes

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ db = SQLAlchemy(app)  # creates the db object using the configuration
 login = LoginManager(app)
 login.login_view = 'login'
 
-from forms import ContactForm
-from models import Contact
+from forms import ContactForm, LoginForm, RegistrationForm, ResetPasswordForm
+from models import Contact, User
 
 @app.route('/')
 def home():  # put application's code here
@@ -35,6 +35,51 @@ def contact():
         return redirect(url_for("home"))
     return render_template("contactus.html", title="Contact Us", form=form, user=current_user)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(email_address=form.email_address.data, name=form.name.data, user_level=1, active=True) # defaults to regular user
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("/"))
+    return render_template("registration.html", title="User Registration", form=form)
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email_address=form.email_address.data).first()
+        print(user)
+        if user is not None and user.check_password(form.password.data):
+            # User has been authenticated
+            login_user(user)
+            print("DEBUG: Login Successful")
+            return redirect(url_for("/"))
+        else:
+            print("DEBUG: Login Failed")
+            # Username or password incorrect
+            return redirect(url_for("login"))
+    return render_template("login.html", title="Login", form=form)
+
+@app.route('/passwordreset', methods=['GET', 'POST'])
+@login_required
+def reset_password():
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email_address=current_user.email_address).first()
+        user.set_password(form.new_password.data)
+        db.session.commit()
+        flash("Your password has been changed.")
+        return redirect(url_for("/"))
+    return render_template("passwordreset.html", title='Reset Password', form=form, user=current_user)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("/"))
 
 if __name__ == '__main__':
     app.run()
